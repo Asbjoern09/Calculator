a0d689412faa1a1756bf03344541cd92e9bd20bb
Asbjørn <asbjorn454@gmail.com>
2023-01-26 11:39:34 +0100

    Addition function

--- CalculatorScript.js
+++ CalculatorScript.js
@@ -1,13 +1,31 @@
 
 let pressedNumber;
+let pressedNumberArray = [];
+let numberModifier;
+let labelContent;
+let addedNumbers = [];
 function registerNumberInput(pressedNumber){
-    document.getElementById("numberLabel").innerHTML += pressedNumber
+    document.getElementById("numberLabel").innerHTML += pressedNumber;
+    pressedNumberArray.push(pressedNumber)
     console.log(pressedNumber)
-    console.log(document.getElementById("numberLabel").innerHTML)
+    console.log(pressedNumberArray)
 }
 
-function addNumbers(){
-    if(document.getElementById("numberLabel").includes("2")){
-        consolelog("yes");
+
+
+function Calculate(){
+    labelContent = document.getElementById("numberLabel").textContent
+    
+    let wherePlusAt = labelContent.indexOf("+")
+    if(labelContent.includes("+")){
+        addedNumbers = labelContent.split('+')
+        for (let i = 0; i<addedNumbers.length; i++) {
+           addedNumbers[i] = parseInt(addedNumbers[i])
+        }
+        let sum = addedNumbers[0] + addedNumbers[1]
+        console.log(addedNumbers)
+        console.log(sum)
+        return sum;
     }
+    
 }
