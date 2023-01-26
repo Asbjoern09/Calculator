642262521c361c3f0f9db40fd8a14675f741a851
Asbjørn <asbjorn454@gmail.com>
2023-01-26 12:05:03 +0100

    It can now do simple math.
    Still have to implement math rules

--- CalculatorScript.js
+++ CalculatorScript.js
@@ -4,6 +4,9 @@ let pressedNumberArray = [];
 let numberModifier;
 let labelContent;
 let addedNumbers = [];
+let subtractedNumbers = [];
+let multipliedNumbers = [];
+let dividedNumbers = [];
 function registerNumberInput(pressedNumber){
     document.getElementById("numberLabel").innerHTML += pressedNumber;
     pressedNumberArray.push(pressedNumber)
@@ -15,17 +18,54 @@ function registerNumberInput(pressedNumber){
 
 function Calculate(){
     labelContent = document.getElementById("numberLabel").textContent
-    
-    let wherePlusAt = labelContent.indexOf("+")
-    if(labelContent.includes("+")){
-        addedNumbers = labelContent.split('+')
+
+    //while(labelContent.includes("*") || labelContent.includes("+") || labelContent.includes("-")){
+
+    if(labelContent.includes("*")){
+        multipliedNumbers = labelContent.split('*')
+        labelContent = labelContent.replace("*", '');
+        for (let i = 0; i<multipliedNumbers.length; i++) {
+            multipliedNumbers[i] = parseInt(multipliedNumbers[i])
+         }
+         let result = multipliedNumbers[0] * multipliedNumbers[1]
+         console.log(multipliedNumbers)
+         console.log(result)
+         console.log(labelContent)
+    }
+    else if(labelContent.includes("/")){
+        dividedNumbers = labelContent.split('/')
+        labelContent = labelContent.replace("/", '');
+        for (let i = 0; i<dividedNumbers.length; i++) {
+            dividedNumbers[i] = parseInt(dividedNumbers[i])
+         }
+         let result = dividedNumbers[0] / dividedNumbers[1]
+         console.log(dividedNumbers)
+         console.log(result)
+         console.log(labelContent)
+    }
+    else if(labelContent.includes("+")){
+        addedNumbers = labelContent.split('+');
+        labelContent = labelContent.replace("+", '');
         for (let i = 0; i<addedNumbers.length; i++) {
            addedNumbers[i] = parseInt(addedNumbers[i])
         }
         let sum = addedNumbers[0] + addedNumbers[1]
         console.log(addedNumbers)
         console.log(sum)
+        console.log(labelContent)
         return sum;
     }
-    
+    else if(labelContent.includes("-")){
+        labelContent = labelContent.replace("-", '');
+        subtractedNumbers = labelContent.split('-')
+        for (let i = 0; i<subtractedNumbers.length; i++) {
+            subtractedNumbers[i] = parseInt(subtractedNumbers[i])
+         }
+         let result = subtractedNumbers[0] - subtractedNumbers[1]
+         console.log(subtractedNumbers)
+         console.log(result)
+         console.log(labelContent)
+         
+    //}
+}
 }
