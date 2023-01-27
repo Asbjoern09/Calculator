9db7200c80a7d79133c78193cab032f9c4c1e4b0
Asbjørn <asbjorn454@gmail.com>
2023-01-27 11:52:05 +0100

    Calculator is complete

--- CalculatorScript.js
+++ CalculatorScript.js
@@ -3,69 +3,29 @@ let pressedNumber;
 let pressedNumberArray = [];
 let numberModifier;
 let labelContent;
-let addedNumbers = [];
-let subtractedNumbers = [];
-let multipliedNumbers = [];
-let dividedNumbers = [];
+let calculatedLabelContent;
 function registerNumberInput(pressedNumber){
-    document.getElementById("numberLabel").innerHTML += pressedNumber;
-    pressedNumberArray.push(pressedNumber)
-    console.log(pressedNumber)
-    console.log(pressedNumberArray)
-}
-
-
+    labelContent = document.getElementById("numberLabel").textContent
+    if(labelContent.includes("=")){
+        labelContent = labelContent.replace("=",'')
+        console.log(labelContent)
+        document.getElementById("numberLabel").innerHTML = labelContent
+    }
+    
+    document.getElementById("numberLabel").innerHTML += pressedNumber;}
 
 function Calculate(){
     labelContent = document.getElementById("numberLabel").textContent
-
-    //while(labelContent.includes("*") || labelContent.includes("+") || labelContent.includes("-")){
-
-    if(labelContent.includes("*")){
-        multipliedNumbers = labelContent.split('*')
-        labelContent = labelContent.replace("*", '');
-        for (let i = 0; i<multipliedNumbers.length; i++) {
-            multipliedNumbers[i] = parseInt(multipliedNumbers[i])
-         }
-         let result = multipliedNumbers[0] * multipliedNumbers[1]
-         console.log(multipliedNumbers)
-         console.log(result)
-         console.log(labelContent)
+    if(!labelContent){
+        return;
+    }else if(labelContent.includes("=")){
+        return;
     }
-    else if(labelContent.includes("/")){
-        dividedNumbers = labelContent.split('/')
-        labelContent = labelContent.replace("/", '');
-        for (let i = 0; i<dividedNumbers.length; i++) {
-            dividedNumbers[i] = parseInt(dividedNumbers[i])
-         }
-         let result = dividedNumbers[0] / dividedNumbers[1]
-         console.log(dividedNumbers)
-         console.log(result)
-         console.log(labelContent)
-    }
-    else if(labelContent.includes("+")){
-        addedNumbers = labelContent.split('+');
-        labelContent = labelContent.replace("+", '');
-        for (let i = 0; i<addedNumbers.length; i++) {
-           addedNumbers[i] = parseInt(addedNumbers[i])
-        }
-        let sum = addedNumbers[0] + addedNumbers[1]
-        console.log(addedNumbers)
-        console.log(sum)
-        console.log(labelContent)
-        return sum;
-    }
-    else if(labelContent.includes("-")){
-        labelContent = labelContent.replace("-", '');
-        subtractedNumbers = labelContent.split('-')
-        for (let i = 0; i<subtractedNumbers.length; i++) {
-            subtractedNumbers[i] = parseInt(subtractedNumbers[i])
-         }
-         let result = subtractedNumbers[0] - subtractedNumbers[1]
-         console.log(subtractedNumbers)
-         console.log(result)
-         console.log(labelContent)
+    console.log(labelContent)
+    calculatedLabelContent = eval(labelContent)
+    console.log(calculatedLabelContent)
+    document.getElementById("numberLabel").innerHTML = "= " + calculatedLabelContent;}
          
-    //}
-}
-}
+
+function Clear(){
+    document.getElementById("numberLabel").innerHTML = '';}
